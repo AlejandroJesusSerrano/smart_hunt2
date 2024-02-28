e1a01d8987c08af88351638d3df6f2d5cbc4e0ed
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2024-02-28 18:45:19 -0300

    add bootstrap & bootstrap-icons

--- .history/config/db_20240227085748.py
+++ .history/config/db_20240227085748.py
@@ -4,12 +4,10 @@ POSTGRESQL = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'smartHunt2',
     'USER': 'postgres',
-
     #!EN CASA:
     # 'PASSWORD': 'tecnico'
     #!EN TRABAJO:
     'PASSWORD': 'Tecnico+123',
-
     'HOST': 'localhost',
     'PORT': '5432',
     'OPTIONS': {
